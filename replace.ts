c74fe8055aaef32b1e2596d4caabd5ff52d9ad44
stackinspector <stackinspector@126.com>
2021-11-13 00:27:09 +0800

    replace script

---
+++ replace.ts
@@ -0,0 +1,14 @@
+// deno-lint-ignore-file camelcase
+
+const list = ["index.html", "style.css", "main.js"]
+const target_dir = Deno.args[0] as string
+
+for (const filename of list) {
+    Deno.writeTextFileSync(
+        target_dir + filename,
+        Deno.readTextFileSync("./" + filename).replaceAll(
+            "/assert/image",
+            "https://cdn.jsdelivr.net/gh/stackinspector/ldtstore-assert"
+        )
+    )
+}
