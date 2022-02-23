8d76f52ce7e287c661a6911bf70d1408521f73c9
carrotgeball <dz5350@live.cn>
2022-02-23 15:46:16 +0800

    refactor size calculating

--- main.ts
+++ main.ts
@@ -24,13 +24,6 @@ const tools_index = load_json("tools_index")
 const tools_all = load_json("tools_all");
 const tools_cross = load_json("tools_cross");
 
-const OFFSET_LIT = 13;
-// TODO 这里的长度和major中的left一样 添加新的pagetype记得修改这里
-const OFFSET = {
-    "home": 33,
-    "tool": 38,
-}[PAGE_TYPE];
-
 const copy = (text: string) => {
     navigator.clipboard.writeText(text);
 };
@@ -43,32 +36,32 @@ const enum LayoutMode {
 
 let layoutMode = LayoutMode.PC;
 
-const SideState: {
-    distance: number,
-    on: boolean,
-    center: boolean,
-    id: string | null,
-    changing: boolean,
+//与改recalculate有关的数据
+const recalculateResult : {
+    sidePosition: number,           //side的位置
+    distance: number,               //打开侧边栏时需要移动的距离
+    center: boolean                 //侧边栏居中模式（居中时主栏隐藏）
 } = {
+    sidePosition: 0,
     distance: 300,
-    on: false,
-    center: false,
-    id: null,
-    changing: false,
+    center: false
 };
 
-// 在手机端输入框弹出的时候锁定缩放计算 避免画面抖动
-let searchFocus = false;
+const SideState: {
+    on: boolean,                    //侧边栏是否打开
+    id: string | null,              //侧边栏当前id
+} = {
+    on: false,
+    id: null,
+};
 
 // 左滑返回
 let touchX = 0;
 let touchY = 0;
-
 window.ontouchstart = (e: TouchEvent) => {
     touchX = e.touches[0].clientX;
     touchY = e.touches[0].clientY;
 };
-
 window.ontouchend = (e: TouchEvent) => {
     const x = e.changedTouches[0].clientX;
     const y = e.changedTouches[0].clientY;
@@ -77,8 +70,8 @@ window.ontouchend = (e: TouchEvent) => {
     }
 };
 
+//点击背景事件绑定
 content.onclick = background.onclick = (e) => {
-    // 背景点击事件绑定位置变了，这里用来阻止冒泡
     if (
         e.composedPath()[0] === content ||
         e.composedPath()[0] === background
@@ -87,12 +80,20 @@ content.onclick = background.onclick = (e) => {
     }
 };
 
-/**
- * 关闭侧边栏
- */
-const sideClose = () => {
-    sideSet(null);
-};
+//右上角图标事件绑定 （tool only）
+if (PAGE_TYPE === "tool") {
+    search!.onclick = (e: MouseEvent) => {
+        // 用来阻止冒泡
+        e.stopPropagation();
+        sideSet("search");
+    };
+
+    back!.onclick = (e: MouseEvent) => {
+        // 用来阻止冒泡
+        e.stopPropagation();
+        location.href = "/";
+    };
+}
 
 /**
  * 侧边栏移动
@@ -101,18 +102,130 @@ const sideClose = () => {
 const sideMove = (enable: boolean) => {
     if (SideState.on !== enable) {
         SideState.on = enable;
-        SideState.changing = true;
 
-        if (enable) {
-            offset.style.left = `${-SideState.distance}px`;
-            side.style.left = `calc(50% + ${layoutMode === LayoutMode.PC ? OFFSET : OFFSET_LIT}em - ${SideState.distance}px)`;
-        } else {
-            offset.style.left = "0";
-            side.style.left = `calc(50% + ${layoutMode === LayoutMode.PC ? OFFSET : OFFSET_LIT}em)`;
+        if (!enable) {
             SideState.id = null;
+        }
+        positionSet();
+    }
+};
+
+const setTransitionDuration = (time : number = 0.4) => {
+    offset.style.transitionDuration = side.style.transitionDuration = time + "s";
+}
+
+/**
+ * 侧边栏、主栏位置设置
+ */
+const positionSet = () =>{
+    if(SideState.on){
+        offset.style.left = `${-recalculateResult.distance}px`;
+        side.style.left = `${recalculateResult.sidePosition - recalculateResult.distance}px`;
+    }
+    else{
+        offset.style.left = "0";
+        side.style.left = `${recalculateResult.sidePosition}px`;
+    }
+}
+
+/**
+ * 侧边栏内容设置、透明度修改
+ * @param id 要设置为的目标侧边栏id
+ */
+const sideChange = (id: string | null) => {
+    const enable = id !== null;
+    if (enable) {
+        renderSide(id!);
+    }
+    side.style.opacity = enable ? "1" : "0";
+
+    // 防止横向的在侧边栏展开的情况下还能被点到
+    offset.style.visibility = side.style.visibility = "visible";
+    if (recalculateResult.center) {
+        offset.style.opacity = SideState.id === null ? "1" : "0";
+    }
+    else{
+        offset.style.opacity = "1";
+    }
+
+    if (id === "search") {
+        // console.log("focus");
+        let keyword = document.getElementById("keyword") as HTMLInputElement;
+        const inputTrigger = document.getElementById("inputTrigger") as HTMLElement;
+        inputTrigger.onclick = (e) => { keyword.focus(); };
+        keyword.addEventListener("keyup", () => {
+            renderSearch(keyword.value);
+        });
+        keyword.focus();
+    }
+};
+
+/**
+ * 点击侧边栏
+ * @param id 要设置为的目标侧边栏id
+ */
+const sideClick = (id: string) => {
+    sideSet(id);
+};
+
+/**
+ * 点击侧边栏(tool)
+ * @param id 要设置为的目标侧边栏id
+ */
+const toolSideClick = (id: string) => {
+    sideSet(`tool-${id}`);
+};
+
+/**
+ * 关闭侧边栏
+ */
+ const sideClose = () => {
+    sideSet(null);
+};
+
+/**
+ * 设置侧边栏
+ * @param id 要设置为的目标侧边栏id，null时关闭侧边栏
+ */
+const sideSet = (id: string | null) => {
+    setTransitionDuration();
+
+    if (id === null) {
+        //1 关闭
+        SideState.id = null;
+        sideMove(false);
+        sideChange(null);
+        return;
+    }
+
+    if (!SideState.on) {
+        //2 直接打开
+        SideState.id = id;
+        sideMove(true);
+        sideChange(SideState.id);
+    } else {
+        if (SideState.id === id) {
+            //3 两次点击 关闭
+            sideSet(null);
+        } else {
+            //4 点击另一个 切换
+            SideState.id = id;
+            sideChange(null);
         }
     }
 };
+
+side.addEventListener("transitionend", (e) => {
+    if (e.propertyName === "opacity") {
+        // 防止横向的在侧边栏展开的情况下还能被点到
+        offset.style.visibility = offset.style.opacity === "0" ? "hidden" : "visible";
+        side.style.visibility = side.style.opacity === "0" ? "hidden" : "visible";
+        //完成切换的操作
+        if (side.style.opacity === "0" && SideState.id !== null) {
+            sideChange(SideState.id);
+        }
+    }
+});
 
 const cloneTemplate = (template: string) => {
     return (document.getElementById(template) as HTMLTemplateElement).content.cloneNode(true) as DocumentFragment;
@@ -166,132 +279,7 @@ const renderSearch = (keywordText: string) => {
     // console.log(`NO at ${keywordText}`)
 };
 
-/**
- * 侧边栏内容设置、透明度修改
- * @param id 要设置为的目标侧边栏id
- */
-const sideChange = (id: string | null) => {
-    const enable = id !== null;
-    if (enable) {
-        renderSide(id!);
-    }
-    side.style.opacity = enable ? "1" : "0";
-
-    // 防止横向的在侧边栏展开的情况下还能被点到
-    major.style.visibility = side.style.visibility = "visible";
-    if (SideState.center) {
-        // 解决点标题major会闪回一次的问题
-        if (enable) {
-            major.style.opacity = "0";
-        } else {
-            if (SideState.id !== null) {
-                major.style.opacity = "0";
-            } else {
-                major.style.opacity = "1";
-            }
-        }
-    }
-
-    if (id === "search") {
-        // console.log("focus");
-        const keyword = document.getElementById("keyword") as HTMLInputElement;
-        // keyword.focus();
-        keyword.addEventListener("keyup", () => {
-            renderSearch(keyword.value);
-        });
-    }
-};
-
-/**
- * 点击侧边栏
- * @param id 要设置为的目标侧边栏id
- */
-const sideClick = (id: string) => {
-    sideSet(id);
-};
-
-const toolSideClick = (id: string) => {
-    sideSet(`tool-${id}`);
-};
-
-if (PAGE_TYPE === "tool") {
-    /**
-     * 点击搜索栏
-     */
-    search!.onclick = (e: MouseEvent) => {
-        // 用来阻止冒泡
-        e.stopPropagation();
-        sideSet("search");
-    };
-
-    back!.onclick = (e: MouseEvent) => {
-        // 用来阻止冒泡
-        e.stopPropagation();
-        location.href = "/";
-    };
-}
-
-/**
- * 设置侧边栏
- * @param id 要设置为的目标侧边栏id，null时关闭侧边栏
- */
-const sideSet = (id: string | null) => {
-    if (id === null) {
-        SideState.id = null;
-        sideMove(false);
-        sideChange(null);
-        return;
-    }
-
-    if (!SideState.on) {
-        // 直接打开
-        SideState.id = id;
-        sideMove(true);
-        sideChange(SideState.id);
-        searchFocus = id === "search";
-    } else {
-        if (SideState.id === id) {
-            // 两次点击 关闭
-            SideState.id = null;
-            sideMove(false);
-            sideChange(null);
-            searchFocus = false;
-        } else {
-            // 点击另一个 切换
-            SideState.id = id;
-            sideChange(null);
-            searchFocus = id === "search";
-        }
-    }
-};
-
-side.addEventListener("transitionend", (e) => {
-    if (e.propertyName === "opacity") {
-        if (major.style.opacity === "0") {
-            // 防止透明后还能被点到
-            major.style.visibility = "hidden";
-        }
-        if (side.style.opacity === "0") {
-            side.style.visibility = "hidden";
-        }
-    }
-    if (e.propertyName === "left") {
-        recalculate();
-        if (SideState.changing) {
-            SideState.changing = false;
-        }
-    } else if (e.propertyName === "opacity") {
-        if (side.style.opacity === "0" && SideState.id !== null) {
-            sideChange(SideState.id);
-        }
-    }
-});
-
 const recalculate = () => {
-    if (searchFocus) {
-        return;
-    }
-
     // 判定平台，和css对应
     if (body.clientWidth > 800) {
         layoutMode = LayoutMode.PC;
@@ -301,10 +289,6 @@ const recalculate = () => {
         layoutMode = LayoutMode.Phone;
     }
 
-    // 设置遮罩大小为窗口大小
-    content.style.width = `${body.clientWidth}px`;
-    content.style.height = `${body.clientHeight}px`;
-
     // 计算相对大小
     let scaleW: number;
     let scaleH: number;
@@ -312,71 +296,93 @@ const recalculate = () => {
         scaleW = body.clientWidth / 1056;
         scaleH = body.clientHeight / 900;
     } else {
-        scaleH = body.clientHeight / 880;
+        scaleH = body.clientHeight / 800;
         if (layoutMode === LayoutMode.Pad) {
             scaleW = body.clientWidth / 600;
         } else {
             scaleW = body.clientWidth / 450;
         }
     }
-    major.style.fontSize = side.style.fontSize = `${Math.min(scaleH, scaleW)}em`;
+    body.style.fontSize = `${Math.min(scaleH, scaleW)}em`;
 
-    // 垂直方向：计算major的间距
-    let delta = body.clientHeight - major.clientHeight;
-    delta = delta < 120 ? 120 : delta;
-    delta -= 4;
-    side.style.height = `calc(${body.clientHeight - delta}px - 6em)`;
-    major.style.marginTop = side.style.marginTop = major.style.marginBottom = side.style.marginBottom = delta / 2 + "px";
-
-    // 水平方向：计算side移动的距离
-    delta = body.clientWidth - major.clientWidth - side.clientWidth;
-    SideState.center = false;
+    // 水平方向：重写的距离计算
+    const delta_major = body.clientWidth - major.clientWidth;
+    const delta_side = body.clientWidth - side.clientWidth;
+    const delta = body.clientWidth - major.clientWidth - side.clientWidth;
+    //计算side的位置，保证其在major右边，并且最多不超过屏幕宽度
+    if(delta_major < 0){
+        recalculateResult.sidePosition = body.clientWidth;
+    }
+    else{
+        recalculateResult.sidePosition = major.clientWidth + delta_major / 2;
+    }
+    
+    //计算side移动的距离
+    recalculateResult.center = false;
     if (delta > 0) {
-        SideState.distance = major.offsetLeft - delta / 2;
+        recalculateResult.distance = major.offsetLeft - delta / 2;
     } else {
-        const delta2 = body.clientWidth - major.clientWidth;
-        // TODO 添加新的pagetype记得修改这里
-        if (PAGE_TYPE === "tool" && delta2 < 1) {
-            SideState.center = true;
-            delta = body.clientWidth - side.clientWidth;
-            SideState.distance = side.offsetLeft - delta / 2;
+        if (PAGE_TYPE === "tool" && delta_major < 1) {
+            recalculateResult.center = true;
+            recalculateResult.distance = side.clientWidth + delta_side / 2;
         } else {
-            if (layoutMode === LayoutMode.Phone) {
-                delta = body.clientWidth - side.clientWidth;
-                SideState.center = true;
-                SideState.distance = major.clientWidth + major.offsetLeft - delta / 2;
+            if (delta_side < 200) {
+                recalculateResult.center = true;
+                recalculateResult.distance = major.clientWidth + (- major.clientWidth + side.clientWidth) / 2;
             } else {
-                SideState.distance = -delta + major.offsetLeft;
+                recalculateResult.distance = -delta + major.offsetLeft;
             }
         }
     }
+
+    //立即对布局做出响应
+    setTransitionDuration(0);
+    positionSet();
+    if(SideState.on){
+        offset.style.opacity = recalculateResult.center ? "0" : "1";
+        offset.style.visibility = offset.style.opacity === "0" ? "hidden" : "visible";
+    }
+    
 };
 
 window.onresize = () => {
-    if (searchFocus) {
-        return;
-    }
 
     recalculate();
-
-    if (SideState.on) {
-        SideState.id = null;
-        sideClose();
-    } else {
-        side.style.left = `calc(50% + ${layoutMode === LayoutMode.PC ? OFFSET : OFFSET_LIT}em)`;
-    }
+    
 };
 
+/**
+ * item展开
+ * @param e 操作的item
+ */
 const showDetail = (e: HTMLElement) => {
     // console.log(e);
     const content = e.getElementsByClassName("detail-container")[0] as HTMLElement;
     const icon = e.getElementsByClassName("icon-line")[0] as HTMLElement;
-    if (content.clientHeight !== 0) {
+    const height = e.getElementsByClassName("detail")[0].clientHeight;
+    
+    if(content.style.height === "")
+        //第一次操作
         content.style.height = "0px";
-        icon.style.transform = "rotate(0deg)";
-    } else {
-        const height = e.getElementsByClassName("detail")[0].clientHeight;
+    //使用百分比高度-无法应用渐变动画，不使用百分比高度-无法自适应行数变化
+    //这里只能投机取巧在无法被察觉的情况下进行切换
+    if (content.style.height === "100%") {
+        //展开 -> 关闭
+        content.style.transitionDuration = ".01s";
         content.style.height = `${height}px`;
+        content.ontransitionend = () =>{
+            content.style.transitionDuration = ".3s";
+            content.style.height = "0px";
+        }
+        icon.style.transform = "rotate(0deg)";
+    }
+    if (content.style.height === "0px") {
+        //关闭 -> 展开
+        content.style.transitionDuration = ".3s";
+        content.style.height = `${height}px`;
+        content.ontransitionend = () =>{
+            content.style.height = "100%";
+        };
         icon.style.transform = "rotate(90deg)";
     }
 };
