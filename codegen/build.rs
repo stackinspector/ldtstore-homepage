2880b6bdf0f0fb036d08f3be3b0671461be30c7d
stackinspector <stackinspector@126.com>
2023-06-18 16:17:31 +0800

    adjust

--- codegen/build.rs
+++ codegen/build.rs
@@ -1,14 +1,8 @@
 use std::{fs, path::Path};
 
-macro_rules! load {
-    ($p:expr) => {
-        fs::read_to_string($p).unwrap()
-    };
-}
-
 fn read_commit<P: AsRef<Path>>(base_path: P) -> String {
     let base_path = base_path.as_ref();
-    let head = load!(base_path.join(".git/HEAD"));
+    let head = fs::read_to_string(base_path.join(".git/HEAD")).unwrap();
     let head = head.split('\n').next().unwrap();
     let head = head.split("ref: ").nth(1).unwrap();
     let commit = fs::read(base_path.join(".git").join(head)).unwrap();
--- codegen/lib.rs
+++ codegen/lib.rs
@@ -2,10 +2,11 @@
 
 pub use lighthtml::ByteString;
 pub use aho_corasick::AhoCorasick;
-pub type Map<T> = indexmap::IndexMap<ByteString, T>; // Vec<(String, T)>;
+pub type Map<T> = indexmap::IndexMap<ByteString, T>;
 pub type Inserts = Vec<(ByteString, ByteString)>;
 
 pub use concat_string::concat_string as cs;
+
 #[macro_export]
 macro_rules! s {
     ($s:expr) => {
@@ -15,20 +16,24 @@ macro_rules! s {
         $crate::ByteString::from($crate::cs!($($s),+))
     }
 }
+
 #[macro_export]
 macro_rules! add_insert {
     ($insert:ident: $($($s1:expr),+ => $($s2:expr),+)*) => {
         $($insert.push(($crate::s!($($s1),+), $crate::s!($($s2),+)));)*
     };
 }
+
 pub fn insert(input: &str, inserts: Inserts) -> String {
     let (patterns, replaces): (Vec<_>, Vec<_>) = inserts.into_iter().unzip();
     AhoCorasick::new(patterns).unwrap().replace_all(input, &replaces)
 }
+
 pub struct GlobalReplacer {
     replacer: AhoCorasick,
     replaces: Vec<&'static str>,
 }
+
 impl GlobalReplacer {
     pub fn build(patterns: &'static [&'static str], replaces: &[&'static str]) -> GlobalReplacer {
         GlobalReplacer { replacer: AhoCorasick::new(patterns).unwrap(), replaces: replaces.to_vec() }
@@ -38,6 +43,7 @@ impl GlobalReplacer {
         self.replacer.replace_all(input, &self.replaces)
     }
 }
+
 pub mod config;
 pub mod data;
 pub mod codegen;
