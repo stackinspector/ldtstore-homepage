d47db2b7be42210754200c1a016e70563b9af164
stackinspector <stackinspector@126.com>
2024-10-08 17:12:19 +0800

    bump copyright year

--- codegen/lib.rs
+++ codegen/lib.rs
@@ -141,7 +141,7 @@ impl FileType {
 }
 
 const COPYRIGHT_L: &str = "
-  Copyright (c) 2021-2023 CarrotGeball and stackinspector. All rights reserved. MIT license.
+  Copyright (c) 2021-2024 CarrotGeball and stackinspector. All rights reserved. MIT license.
   Source: https://github.com/stackinspector/ldtstore-homepage
   Commit (content): ";
 
